e8d066765a1d20b3aa5ddb9fe6fab41e65f94f10
bonyfa <arsens818@gmail.com>
2024-12-09 23:33:52 +0400

    Add function get_html code and random user agent
    1 функция достает код с сайта ,  а вторая для обхода защиты от ботов , запроса отправляются через рандомные промежутки времени

--- lab_3/parser.py
+++ lab_3/parser.py
@@ -10,6 +10,8 @@ from fake_useragent import UserAgent
 from typing import List
 
 
+URL = "https://akbotadoya.ru/akkumulyatory?page="
+
 def parse_arguments() -> argparse:
     """
             Получаем ссылку путь директории и количество страниц
@@ -20,3 +22,35 @@ def parse_arguments() -> argparse:
     parser.add_argument("--urls", type=str, default="https://akbotadoya.ru/akkumulyatory?page=", help="Базовый URL для сбора данных")
     parser.add_argument("--pages", type=int, default=3, help="Количество страниц для обхода")
     return parser.parse_args()
+
+def get_html_code(page: int , url: str) -> BeautifulSoup:
+    """
+               Получаем html код страницы.
+
+    """
+
+    try:
+        tmp_url = url+str(page)
+        sleep_time = random.uniform(1, 3)
+        sleep(sleep_time)
+        headers = {
+            "User-Agent": random_user_agent()
+        }
+        tmp_res = requests.get(tmp_url, headers=headers)
+        tmp_res.raise_for_status()
+        soup = BeautifulSoup(tmp_res.content, "html.parser")
+        return soup
+    except requests.exceptions.RequestException as e:
+        logging.exception(f"Ошибка при получении html кода")
+        return None
+
+def random_user_agent() -> str:
+    u = UserAgent()
+    return u.random
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    string = get_html_code(1, URL)
+    print(string)
+    print("sdad")
+
